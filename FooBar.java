01cc67d924988ba601556848c97defeb2be8259e
bogus <boguslaw.brzezny@gmail.com>
2019-09-16 21:50:05 +0200

    zadanie 6. obliczanie silni

--- FooBar.java
+++ FooBar.java
@@ -1,22 +1,27 @@
 package bb.bbjava.poczatek.FooBar;
-
+// zadanie 6. troche pomoglo mi Udemy, bo tam byl przyklad z silnia na rekurencje ;-)
 public class FooBar {
 
-
     public static void main(String[] args) {
-        for (int i = 1; i < 101; i++) {
+        //oblicza silnie przez rekurencję
+        System.out.println(silnia(6));
 
-            if (i % 2 == 0 && i % 5 == 0) {
-                System.out.println(" FooBar");
-            } else if (i % 2 == 0) {
-                System.out.println(" Foo");
-            } else if (i % 5 == 0) {
-                System.out.println(" Bar");
-            }
-            if (i % 3 == 0) {
-                System.out.println(" Cep");
-            }
+        // oblicza silnię w pętli
+        System.out.println(siloop(6));
+    }
+
+    private static long silnia(long n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * silnia(n - 1);
         }
-
+    }
+    private static long siloop(long n) {
+        long k = 1;
+        for (int i = 1; i <= n; i++) {
+            k = k * i;
+        }
+        return k;
     }
 }
